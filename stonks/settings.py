08efc8e5a828af1c73e48fda2571ceef2c554565
thecoderenroute <thecoderenroute@gmail.com>
2022-09-03 17:46:39 +0530

    minor change in settings

--- stonks/settings.py
+++ stonks/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '06ui(cp1ml%))$r9ay=_n6yuq+or^jmp0mqcit^a^-53#+d=wm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stonks',
+    'main'
 ]
 
 MIDDLEWARE = [
