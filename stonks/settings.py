e3f44abb5e22343b81dbb7fe5aa293deeca5218c
thecoderenroute <thecoderenroute@gmail.com>
2022-09-04 16:49:35 +0530

    added mimetype html

--- stonks/settings.py
+++ stonks/settings.py
@@ -15,6 +15,8 @@ import os
 
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
+mimetypes.add_type("text/html", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
